94fed123abfa73792c4d56e8bf59fb998c0768da
Cheena Malhotra <v-chmalh@microsoft.com>
2018-12-18 12:58:05 -0800

    Add CredentialManager

---
+++ src/main/java/com/test/aadmsi/msiexample/CredentialManager.java
@@ -0,0 +1,13 @@
+package com.test.aadmsi.msiexample;
+
+public class CredentialManager {
+    static String AZURE_SQL_SPN = "https://database.windows.net/";
+    static String AZURE_RESOURCE_SPN = "https://management.azure.com";
+    static String hostNameInCertificate = "*.database.windows.net"; // Host Name in Certificate
+    static String ActiveDirectoryMSI = "ActiveDirectoryMSI"; // Authentication Mode for MSI Authentication
+
+    // Provide Connection details below:
+    static String serverName = ""; // Server URL xxxxxxx.database.windows.net
+    static String databaseName = ""; // Name of database
+    static String userMSIClientId = ""; // (Optional) Client ID of User Assigned MSI
+}
